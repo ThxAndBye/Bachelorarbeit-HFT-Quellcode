05c55881c1888358d5e207967cb92a34d32e25d7
Thx And Bye <16294365+ThxAndBye@users.noreply.github.com>
2021-06-05 15:24:20 +0200

    added vscode launch and settings config for debugging

---
+++ NRF52DK/nimble_gatt_sht3x/bin/nrf52dk/application_nimble_gatt_sht3x/main.d
@@ -0,0 +1,393 @@
+/home/popos/RIOT/examples/nimble_gatt_sht3x/bin/nrf52dk/application_nimble_gatt_sht3x/main.o: \
+ /home/popos/RIOT/examples/nimble_gatt_sht3x/main.c \
+ /home/popos/RIOT/examples/nimble_gatt_sht3x/bin/nrf52dk/riotbuild/riotbuild.h \
+ /home/popos/RIOT/core/include/assert.h \
+ /home/popos/RIOT/core/include/panic.h \
+ /home/popos/RIOT/core/include/kernel_defines.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/nano/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/sys/_stdint.h \
+ /usr/include/newlib/sys/_pthreadtypes.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/stdlib.h \
+ /usr/include/newlib/string.h /usr/include/newlib/sys/string.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_gatt.h \
+ /usr/include/newlib/inttypes.h /usr/include/newlib/sys/_intsup.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_att.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/queue.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_uuid.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/hci_common.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/ble.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/npl/riot/include/syscfg/syscfg.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/os.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/nimble_npl.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/npl/riot/include/nimble/nimble_npl_os.h \
+ /home/popos/RIOT/sys/include/event/callback.h \
+ /home/popos/RIOT/sys/include/event.h \
+ /home/popos/RIOT/core/include/clist.h \
+ /home/popos/RIOT/core/include/list.h /home/popos/RIOT/core/include/irq.h \
+ /home/popos/RIOT/cpu/nrf52/include/cpu_conf.h \
+ /home/popos/RIOT/cpu/cortexm_common/include/cpu_conf_common.h \
+ /home/popos/RIOT/cpu/nrf52/include/vendor/nrf52.h \
+ /home/popos/RIOT/cpu/cortexm_common/include/vendor/core_cm4.h \
+ /home/popos/RIOT/cpu/cortexm_common/include/vendor/cmsis_version.h \
+ /home/popos/RIOT/cpu/cortexm_common/include/vendor/cmsis_compiler.h \
+ /home/popos/RIOT/cpu/cortexm_common/include/vendor/cmsis_gcc.h \
+ /home/popos/RIOT/cpu/cortexm_common/include/vendor/mpu_armv7.h \
+ /home/popos/RIOT/cpu/nrf52/include/vendor/nrf52_bitfields.h \
+ /home/popos/RIOT/cpu/nrf52/include/vendor/nrf52832_peripherals.h \
+ /home/popos/RIOT/cpu/cortexm_common/include/irq_arch.h \
+ /home/popos/RIOT/core/include/thread.h \
+ /home/popos/RIOT/core/include/clist.h \
+ /home/popos/RIOT/core/include/cib.h \
+ /home/popos/RIOT/core/include/assert.h \
+ /home/popos/RIOT/core/include/msg.h \
+ /home/popos/RIOT/core/include/sched.h \
+ /home/popos/RIOT/core/include/native_sched.h \
+ /home/popos/RIOT/core/include/thread_config.h \
+ /home/popos/RIOT/core/include/thread_flags.h \
+ /home/popos/RIOT/cpu/cortexm_common/include/thread_arch.h \
+ /home/popos/RIOT/core/include/thread_flags.h \
+ /home/popos/RIOT/sys/include/ptrtag.h \
+ /home/popos/RIOT/sys/include/ztimer.h \
+ /home/popos/RIOT/core/include/sched.h \
+ /home/popos/RIOT/core/include/msg.h \
+ /home/popos/RIOT/core/include/mutex.h \
+ /home/popos/RIOT/core/include/thread.h \
+ /home/popos/RIOT/core/include/irq.h \
+ /home/popos/RIOT/core/include/rmutex.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdatomic.h \
+ /home/popos/RIOT/core/include/mutex.h \
+ /home/popos/RIOT/sys/include/sema.h \
+ /home/popos/RIOT/sys/include/ztimer.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/endian.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/os_error.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/os_mbuf.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/os_mempool.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_eddystone.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_gap.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_adv.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_id.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/ble.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_hci.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_log.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/nimble/include/modlog/modlog.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/nimble/include/log_common/log_common.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/nimble/include/log_common/ignore.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/nimble/include/log/log.h \
+ /home/popos/RIOT/build/pkg/nimble/porting/npl/riot/include/logcfg/logcfg.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_mbuf.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_stop.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_ibeacon.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_l2cap.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/nimble_opt.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/nimble_opt_auto.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_sm.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_store.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/util/include/host/util/util.h \
+ /home/popos/RIOT/sys/include/net/bluetil/ad.h \
+ /home/popos/RIOT/sys/include/net/ble.h \
+ /home/popos/RIOT/pkg/nimble/contrib/include/nimble_riot.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/services/gap/include/services/gap/ble_svc_gap.h \
+ /home/popos/RIOT/build/pkg/nimble/nimble/host/services/gatt/include/services/gatt/ble_svc_gatt.h \
+ /home/popos/RIOT/drivers/include/sht3x.h \
+ /home/popos/RIOT/drivers/include/periph/i2c.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include-fixed/limits.h \
+ /home/popos/RIOT/boards/nrf52dk/include/periph_conf.h \
+ /home/popos/RIOT/boards/common/nrf52xxxdk/include/periph_conf_common.h \
+ /home/popos/RIOT/cpu/nrf52/include/periph_cpu.h \
+ /home/popos/RIOT/cpu/nrf5x_common/include/periph_cpu_common.h \
+ /home/popos/RIOT/cpu/cortexm_common/include/cpu.h \
+ /home/popos/RIOT/boards/common/nrf52/include/cfg_clock_32_1.h \
+ /home/popos/RIOT/boards/common/nrf52/include/cfg_i2c_default.h \
+ /home/popos/RIOT/boards/common/nrf52/include/cfg_rtt_default.h \
+ /home/popos/RIOT/boards/common/nrf52/include/cfg_timer_default.h \
+ /home/popos/RIOT/boards/nrf52dk/include/board.h \
+ /home/popos/RIOT/boards/common/nrf52/include/board_common.h \
+ /home/popos/RIOT/drivers/sht3x/include/sht3x_params.h \
+ /home/popos/RIOT/sys/include/saul_reg.h \
+ /home/popos/RIOT/drivers/include/saul.h /usr/include/newlib/errno.h \
+ /usr/include/newlib/sys/errno.h /home/popos/RIOT/sys/include/phydat.h \
+ /home/popos/RIOT/core/include/kernel_defines.h \
+ /home/popos/RIOT/sys/include/phydat.h
+
+/home/popos/RIOT/examples/nimble_gatt_sht3x/bin/nrf52dk/riotbuild/riotbuild.h:
+
+/home/popos/RIOT/core/include/assert.h:
+
+/home/popos/RIOT/core/include/panic.h:
+
+/home/popos/RIOT/core/include/kernel_defines.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/usr/include/newlib/stdio.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/nano/newlib.h:
+
+/usr/include/newlib/_newlib_version.h:
+
+/usr/include/newlib/sys/config.h:
+
+/usr/include/newlib/machine/ieeefp.h:
+
+/usr/include/newlib/sys/features.h:
+
+/usr/include/newlib/sys/cdefs.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h:
+
+/usr/include/newlib/sys/reent.h:
+
+/usr/include/newlib/_ansi.h:
+
+/usr/include/newlib/sys/_types.h:
+
+/usr/include/newlib/machine/_types.h:
+
+/usr/include/newlib/sys/lock.h:
+
+/usr/include/newlib/sys/types.h:
+
+/usr/include/newlib/sys/_stdint.h:
+
+/usr/include/newlib/sys/_pthreadtypes.h:
+
+/usr/include/newlib/machine/types.h:
+
+/usr/include/newlib/sys/stdio.h:
+
+/usr/include/newlib/stdlib.h:
+
+/usr/include/newlib/machine/stdlib.h:
+
+/usr/include/newlib/string.h:
+
+/usr/include/newlib/sys/string.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_gatt.h:
+
+/usr/include/newlib/inttypes.h:
+
+/usr/include/newlib/sys/_intsup.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_att.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/queue.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_uuid.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/hci_common.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/ble.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/npl/riot/include/syscfg/syscfg.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/os.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/nimble_npl.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/npl/riot/include/nimble/nimble_npl_os.h:
+
+/home/popos/RIOT/sys/include/event/callback.h:
+
+/home/popos/RIOT/sys/include/event.h:
+
+/home/popos/RIOT/core/include/clist.h:
+
+/home/popos/RIOT/core/include/list.h:
+
+/home/popos/RIOT/core/include/irq.h:
+
+/home/popos/RIOT/cpu/nrf52/include/cpu_conf.h:
+
+/home/popos/RIOT/cpu/cortexm_common/include/cpu_conf_common.h:
+
+/home/popos/RIOT/cpu/nrf52/include/vendor/nrf52.h:
+
+/home/popos/RIOT/cpu/cortexm_common/include/vendor/core_cm4.h:
+
+/home/popos/RIOT/cpu/cortexm_common/include/vendor/cmsis_version.h:
+
+/home/popos/RIOT/cpu/cortexm_common/include/vendor/cmsis_compiler.h:
+
+/home/popos/RIOT/cpu/cortexm_common/include/vendor/cmsis_gcc.h:
+
+/home/popos/RIOT/cpu/cortexm_common/include/vendor/mpu_armv7.h:
+
+/home/popos/RIOT/cpu/nrf52/include/vendor/nrf52_bitfields.h:
+
+/home/popos/RIOT/cpu/nrf52/include/vendor/nrf52832_peripherals.h:
+
+/home/popos/RIOT/cpu/cortexm_common/include/irq_arch.h:
+
+/home/popos/RIOT/core/include/thread.h:
+
+/home/popos/RIOT/core/include/clist.h:
+
+/home/popos/RIOT/core/include/cib.h:
+
+/home/popos/RIOT/core/include/assert.h:
+
+/home/popos/RIOT/core/include/msg.h:
+
+/home/popos/RIOT/core/include/sched.h:
+
+/home/popos/RIOT/core/include/native_sched.h:
+
+/home/popos/RIOT/core/include/thread_config.h:
+
+/home/popos/RIOT/core/include/thread_flags.h:
+
+/home/popos/RIOT/cpu/cortexm_common/include/thread_arch.h:
+
+/home/popos/RIOT/core/include/thread_flags.h:
+
+/home/popos/RIOT/sys/include/ptrtag.h:
+
+/home/popos/RIOT/sys/include/ztimer.h:
+
+/home/popos/RIOT/core/include/sched.h:
+
+/home/popos/RIOT/core/include/msg.h:
+
+/home/popos/RIOT/core/include/mutex.h:
+
+/home/popos/RIOT/core/include/thread.h:
+
+/home/popos/RIOT/core/include/irq.h:
+
+/home/popos/RIOT/core/include/rmutex.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdatomic.h:
+
+/home/popos/RIOT/core/include/mutex.h:
+
+/home/popos/RIOT/sys/include/sema.h:
+
+/home/popos/RIOT/sys/include/ztimer.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/endian.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/os_error.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/os_mbuf.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/nimble/include/os/os_mempool.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_eddystone.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_gap.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_adv.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_id.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/ble.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_hci.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_log.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/nimble/include/modlog/modlog.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/nimble/include/log_common/log_common.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/nimble/include/log_common/ignore.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/nimble/include/log/log.h:
+
+/home/popos/RIOT/build/pkg/nimble/porting/npl/riot/include/logcfg/logcfg.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_mbuf.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_hs_stop.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_ibeacon.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_l2cap.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/nimble_opt.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/include/nimble/nimble_opt_auto.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_sm.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/include/host/ble_store.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/util/include/host/util/util.h:
+
+/home/popos/RIOT/sys/include/net/bluetil/ad.h:
+
+/home/popos/RIOT/sys/include/net/ble.h:
+
+/home/popos/RIOT/pkg/nimble/contrib/include/nimble_riot.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/services/gap/include/services/gap/ble_svc_gap.h:
+
+/home/popos/RIOT/build/pkg/nimble/nimble/host/services/gatt/include/services/gatt/ble_svc_gatt.h:
+
+/home/popos/RIOT/drivers/include/sht3x.h:
+
+/home/popos/RIOT/drivers/include/periph/i2c.h:
+
+/usr/lib/gcc/arm-none-eabi/9.2.1/include-fixed/limits.h:
+
+/home/popos/RIOT/boards/nrf52dk/include/periph_conf.h:
+
+/home/popos/RIOT/boards/common/nrf52xxxdk/include/periph_conf_common.h:
+
+/home/popos/RIOT/cpu/nrf52/include/periph_cpu.h:
+
+/home/popos/RIOT/cpu/nrf5x_common/include/periph_cpu_common.h:
+
+/home/popos/RIOT/cpu/cortexm_common/include/cpu.h:
+
+/home/popos/RIOT/boards/common/nrf52/include/cfg_clock_32_1.h:
+
+/home/popos/RIOT/boards/common/nrf52/include/cfg_i2c_default.h:
+
+/home/popos/RIOT/boards/common/nrf52/include/cfg_rtt_default.h:
+
+/home/popos/RIOT/boards/common/nrf52/include/cfg_timer_default.h:
+
+/home/popos/RIOT/boards/nrf52dk/include/board.h:
+
+/home/popos/RIOT/boards/common/nrf52/include/board_common.h:
+
+/home/popos/RIOT/drivers/sht3x/include/sht3x_params.h:
+
+/home/popos/RIOT/sys/include/saul_reg.h:
+
+/home/popos/RIOT/drivers/include/saul.h:
+
+/usr/include/newlib/errno.h:
+
+/usr/include/newlib/sys/errno.h:
+
+/home/popos/RIOT/sys/include/phydat.h:
+
+/home/popos/RIOT/core/include/kernel_defines.h:
+
+/home/popos/RIOT/sys/include/phydat.h:
